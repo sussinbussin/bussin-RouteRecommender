d2c3425fcb95ac46eee75b55762241c46aac51ea
Wei Bin <auyeongweibin@gmail.com>
2022-10-04 21:13:08 +0800

    added pre google maps api filter

--- application.py
+++ application.py
@@ -12,12 +12,6 @@ def create_app(config_filename):
     from api_links import api_bp
     application.register_blueprint(api_bp, url_prefix='/v1')
 
-    pymysql_connect_kwargs = {'user': 'root',
-                          'password': 'Matcha8$',
-                          'host': '127.0.0.1',
-                          'database': 'gas_prices'}
-
-    app.config['pymysql_kwargs'] = pymysql_connect_kwargs
     mysql = MySQL(app)
 
     return application
--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Google Maps Routes API Key
-# ROUTES_API_KEY = os.environ["ROUTES_API_KEY"]
+ROUTES_API_KEY = os.environ["ROUTES_API_KEY"]
 
 # MySQL Connection Credentials
-# user = os.environ["USER"]
-# password = os.environ["PASSWORD"]
-# host = os.environ["HOST"]
-# database = os.environ["DATABASE"]
+pymysql_connect_kwargs = {'user': os.environ["USER"],
+                        'password': os.environ["PASSWORD"],
+                        'host': os.environ["HOST"],
+                        'database': os.environ["DATABASE"]}
 
 # Deployment instructions
 # https://dev.to/aws-builders/dockerize-an-api-based-flask-app-and-deploy-on-amazon-ecs-2pk0
