50035f567c9f9e24231b6cd4e70d0b2ce7c48cdb
Wei Bin <auyeongweibin@gmail.com>
2022-11-03 17:39:38 +0800

    added authentication

--- config.py
+++ config.py
@@ -2,24 +2,32 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Google Maps Routes API Key
-ROUTES_API_KEY = os.environ["ROUTES_API_KEY"]
+# ROUTES_API_KEY = os.environ["ROUTES_API_KEY"]
 
-CITYMAPPER_API_KEY = os.environ["CITYMAPPER_API_KEY"]
+# CITYMAPPER_API_KEY = os.environ["CITYMAPPER_API_KEY"]
 
-USER = os.environ["USER"] 
-PASSWORD =  os.environ["PASSWORD"]
-HOST = os.environ["HOST"]
-DATABASE = os.environ["DATABASE"]
+# USER = os.environ["USER"] 
+# PASSWORD =  os.environ["PASSWORD"]
+# HOST = os.environ["HOST"]
+# DATABASE = os.environ["DATABASE"]
+# COGNITO_REGION = os.environ["COGNITO_REGION"]
+# COGNITO_USERPOOL_ID = os.environ["COGNITO_USERPOOL_ID"]
+# COGNITO_APP_CLIENT_ID = os.environ["COGNITO_APP_CLIENT_ID"]
+# COGNITO_JWT_HEADER_NAME = "Authorization"
+# COGNITO_JWT_HEADER_PREFIX = "Bearer"
 
 # Deployment instructions
 # https://dev.to/aws-builders/dockerize-an-api-based-flask-app-and-deploy-on-amazon-ecs-2pk0
 
-# ROUTES_API_KEY = ""
+ROUTES_API_KEY = "AIzaSyBgJzxk90783V8Gt0TLN0S-LJrLc310IIc"
 
-# CITYMAPPER_API_KEY = ""
+CITYMAPPER_API_KEY = "gjxZIURYE5DmX49N9eAn5riKyuTcg5C5"
 
-# USER = 'root'
-# # Please fill in your password
-# PASSWORD = ''
-# HOST = '127.0.0.1'
-# DATABASE = 'hewwo'
+USER = 'root'
+# Please fill in your password
+PASSWORD = 'Matcha8$'
+HOST = '127.0.0.1'
+DATABASE = 'hewwo'
+COGNITO_REGION = "ap-southeast-1"
+COGNITO_USERPOOL_ID =  "ap-southeast-1_cAJ9EgqL1"
+COGNITO_APP_CLIENT_ID = "4pv7rad4vqjk19j50hlpg6jleo"
